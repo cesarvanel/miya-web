08c68902ab32ddc47ab897f475f7a562a34e3617
cesarzoleko <cesarzoleko@gmail.com>
2026-07-10 07:47:47 +0100

    setup theme projects

--- apps/bank/src/main.tsx
+++ apps/bank/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import { RouterProvider } from 'react-router-dom';
+import { BankProviders } from './config/providers';
+import { makeBankRouter } from './config/router';
+
+const router = makeBankRouter();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <StrictMode>
-    <App/>
-  </StrictMode>
+    <BankProviders>
+      <RouterProvider router={router} />
+    </BankProviders>
+  </StrictMode>,
 );
